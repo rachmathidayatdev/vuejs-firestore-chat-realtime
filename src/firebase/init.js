1f0e84fb8587add0ffe8f063433eb43768af8f7b
Rachmat Hidayat <37132165+rachmathidayatdev@users.noreply.github.com>
2018-06-18 16:57:15 +0700

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import firestore from 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDsr51G-tJtzF7DAMFCCZy5YmeXU_ogCpg",
-    authDomain: "vuejs-firebase-chat-363d1.firebaseapp.com",
-    databaseURL: "https://vuejs-firebase-chat-363d1.firebaseio.com",
-    projectId: "vuejs-firebase-chat-363d1",
-    storageBucket: "vuejs-firebase-chat-363d1.appspot.com",
-    messagingSenderId: "1016350755124"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId"
 };
 const firebaseApp = firebase.initializeApp(config);
 firebaseApp.firestore().settings({ timestampsInSnapshots: true })
